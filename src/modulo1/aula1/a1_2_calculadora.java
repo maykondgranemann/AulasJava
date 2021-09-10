61483d9102ef206e434cf5d773e94b03fd8225b7
Maykon D. Granemann <maykondgranemann@gmail.com>
2021-09-10 13:38:26 -0300

    A1 - Criado arquivos para leitura e impressao no console

---
+++ src/modulo1/aula1/a1_2_calculadora.java
@@ -0,0 +1,19 @@
+package modulo1.aula1;
+
+import java.util.Scanner;
+
+public class a1_2_calculadora {
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro número: ");
+        int n1 = sc.nextInt();
+        System.out.print("Digite o segundo número: ");
+        int n2 = sc.nextInt();
+
+        int resultado = n1 + n2;
+
+        System.out.println("A soma entre " + n1 + " e " + n2 + " é " + resultado);
+    }
+}
