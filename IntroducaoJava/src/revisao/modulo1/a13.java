7943dc05185a7c55c019ab36f19a99181bfe8b9b
Maykon D. Granemann <maykondgranemann@gmail.com>
2021-09-24 16:49:47 -0300

    Revisao- A13- For encadeado

---
+++ IntroducaoJava/src/revisao/modulo1/a13.java
@@ -0,0 +1,15 @@
+package modulo1;
+
+public class a13 {
+    public static void main(String[] args) {
+
+        for(int linha = 1; linha <= 10; linha ++){
+            System.out.printf("%d - ",linha);
+
+            for(int colunas = 1; colunas <= 10; colunas ++){
+                System.out.printf("%d ",colunas);
+            }
+            System.out.println();
+        }
+    }
+}
