e0a38bed5bcbb8b48f84899927d627339a9d14ac
Maykon D. Granemann <maykondgranemann@gmail.com>
2021-09-24 10:26:10 -0300

    Revisão Aula 6 - If

---
+++ IntroducaoJava/src/revisao/modulo1/a6.java
@@ -0,0 +1,26 @@
+package modulo1;
+
+public class a6 {
+    public static void main(String[] args) {
+        boolean executa = false;
+        if(executa){
+           System.out.println("Entrou no if"); 
+        }
+
+        boolean verdade = true;
+        if(!verdade){
+           System.out.println("Entrou no segundo if"); 
+        }
+        
+        if(!verdade || executa){
+           System.out.println("Entrou no terceiro if"); 
+        }
+
+        if(verdade && !executa){
+            System.out.println("Entrou no quarto if"); 
+        }     
+        
+        boolean resultado = verdade && !executa;
+        
+    }
+}
