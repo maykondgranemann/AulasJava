22adf81267e05f8cbac51ffee7302870a0f7791f
Maykon D. Granemann <maykondgranemann@gmail.com>
2021-09-24 17:08:15 -0300

    Revisao A14 - For decremento

---
+++ IntroducaoJava/src/revisao/modulo1/s14.java
@@ -0,0 +1,17 @@
+package modulo1;
+
+public class s14 {
+   public static void main(String[] args) {
+      for (int i = 0; i < 10; i++) {          
+         System.out.println(i);
+      }
+
+      for(int i = 10; i > 0; i--){
+         System.out.println(i);
+      }  
+      
+      for (int i = 0; i < 10; i++) {
+         System.out.println("Maykon");
+      }
+   } 
+}
