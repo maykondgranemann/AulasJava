c6e4c92c2063b52fa7c99bbbd65359570a9266b3
Maykon D. Granemann <maykondgranemann@gmail.com>
2021-09-24 11:28:58 -0300

    Revisao - A9 - While incremento

---
+++ IntroducaoJava/src/revisao/modulo1/a9.java
@@ -0,0 +1,22 @@
+package modulo1;
+
+public class a9 {
+    public static void main(String[] args) {
+        
+        int repeticoes = 1;
+        while(repeticoes <= 5){            
+            System.out.printf("\nrepetição %d", repeticoes);
+            repeticoes = repeticoes + 1;
+        }
+        repeticoes = 1;
+        while(repeticoes <= 5){            
+            System.out.printf("\nrepetição %d", repeticoes);
+            repeticoes += 1;
+        }
+        repeticoes = 1;
+        while(repeticoes <= 5){            
+            System.out.printf("\nrepetição %d", repeticoes);
+            repeticoes ++;
+        }
+    }
+}
