7a4c9e1bff2689d9704c6e27566d0327a9dc3dc9
Maykon D. Granemann <maykondgranemann@gmail.com>
2021-09-24 17:46:22 -0300

    Revisao - A16 - Switch Case

---
+++ IntroducaoJava/src/revisao/modulo1/a16.java
@@ -0,0 +1,21 @@
+package modulo1;
+
+public class a16 {
+    public static void main(String[] args) {
+        int opcao = 5;
+
+        switch (opcao) {
+            case 1:
+                System.out.println("Igual a 1");
+                break;
+            case 2:
+                System.out.println("Igual a 2");
+                break;  
+            case 3:
+                System.out.println("Igual a 3");
+                break;
+            default:
+                System.out.println("Opcao nao prevista");
+        }
+    }
+}
