5ee099dec7d7f8c7380feba4a224110a0208e9e7
Maykon D. Granemann <maykondgranemann@gmail.com>
2021-09-24 14:11:31 -0300

    Revisao  - A11 - DoWhile

---
+++ IntroducaoJava/src/revisao/modulo1/a11.java
@@ -0,0 +1,24 @@
+package modulo1;
+
+import java.util.Scanner;
+
+public class a11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        byte idade = 0;
+        boolean invalido = false;
+        do{
+            System.out.print("Digite sua idade: ");
+            idade = Byte.parseByte(sc.nextLine());
+            if(idade < 0){
+                System.out.println("Idade nao pode ser negativa!");
+                invalido =true;
+            }
+        }while(invalido);
+
+
+
+        System.out.println("========== Obrigado ===========");
+    }
+}
