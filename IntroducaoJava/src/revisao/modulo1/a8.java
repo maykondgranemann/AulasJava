d724c7fa76f9f405680b97a1747b8c04f8e5efb5
Maykon D. Granemann <maykondgranemann@gmail.com>
2021-09-24 11:08:56 -0300

    Revisao A8 - If else

---
+++ IntroducaoJava/src/revisao/modulo1/a8.java
@@ -0,0 +1,26 @@
+package modulo1;
+
+public class a8 {
+    public static void main(String[] args) {
+        byte idade = 19;
+
+        if(idade >= 17 ){
+            System.out.println("De maior");
+        }
+        else{
+            System.out.println("De menor");
+        }
+
+        if(idade > 18){
+            System.out.println("passou da idade");
+        }
+        else if(idade == 18)
+        {
+            System.out.println("alistamento");
+        }
+        else{
+            System.out.println("abaixo da idade");
+        }
+
+    }
+}
