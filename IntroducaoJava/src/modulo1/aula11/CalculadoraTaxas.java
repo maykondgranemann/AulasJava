8e43a5d0043d76b52dab66c9c97cebcea86fbcfd
Maykon D. Granemann <maykondgranemann@gmail.com>
2021-10-01 09:47:47 -0300

    Metodo de calculo de Taxa de saque - ATP16

--- IntroducaoJava/src/modulo1/aula11/CalculadoraTaxas.java
+++ IntroducaoJava/src/modulo1/aula11/CalculadoraTaxas.java
@@ -1,8 +1,20 @@
 public class CalculadoraTaxas {
     private double taxaTransferencia = 0.00001;
+    private double taxaSaque = 1.3;
+    private int numeroSaques;
     
     public double calculaTaxaTransferencia(double valor){
         double valorTaxa = valor * taxaTransferencia;
+        return valorTaxa;
+    }
+
+    public double calculaTaxaSaque(double valor){
+        numeroSaques++;
+        double valorTaxa = 0;
+
+        if(numeroSaques % 5 == 0 ){
+            valorTaxa = taxaSaque;
+        }  
         return valorTaxa;
     }
 }
