c5109d7e666e5b2ed0642df8f3b488c372feac81
Maykon D. Granemann <maykondgranemann@gmail.com>
2021-10-01 09:55:28 -0300

    Chamada os metodos da calculado na main - ATP16

--- IntroducaoJava/src/modulo1/aula11/AtividadeBanco.java
+++ IntroducaoJava/src/modulo1/aula11/AtividadeBanco.java
@@ -1,5 +1,21 @@
 public class AtividadeBanco {
     public static void main(String[] args) {
-        
+        CalculadoraTaxas calcTaxas = new CalculadoraTaxas();
+
+        double taxaT1 = calcTaxas.calculaTaxaTransferencia(1000);
+        double taxaT2 = calcTaxas.calculaTaxaTransferencia(500);
+
+        double taxaS1 = calcTaxas.calculaTaxaSaque(50);
+        double taxaS2 = calcTaxas.calculaTaxaSaque(150);
+        double taxaS3 = calcTaxas.calculaTaxaSaque(500);
+        double taxaS4 = calcTaxas.calculaTaxaSaque(900);
+        double taxaS5 = calcTaxas.calculaTaxaSaque(600);
+        double taxaS6 = calcTaxas.calculaTaxaSaque(1550);
+
+        System.out.printf("Os valores de taxa de transferencia foram  %.4f e %.4f\n", taxaT1, taxaT2);
+
+        System.out.printf("Os valores de taxa de saque foram  %.2f, %.2f, %.2f, %.2f, %.2f e %.2f\n", 
+            taxaS1, taxaS2, taxaS3, taxaS4, taxaS5, taxaS6);
+
     }
 }
