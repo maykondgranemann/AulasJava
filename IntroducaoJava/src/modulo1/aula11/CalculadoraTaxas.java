60315aa245f1ffdf872e022033c7262c64462351
Maykon D. Granemann <maykondgranemann@gmail.com>
2021-10-01 09:41:04 -0300

    Metodo de calculo de taxa de transferencia - ATP16

--- IntroducaoJava/src/modulo1/aula11/CalculadoraTaxas.java
+++ IntroducaoJava/src/modulo1/aula11/CalculadoraTaxas.java
@@ -1,3 +1,8 @@
 public class CalculadoraTaxas {
+    private double taxaTransferencia = 0.00001;
     
+    public double calculaTaxaTransferencia(double valor){
+        double valorTaxa = valor * taxaTransferencia;
+        return valorTaxa;
+    }
 }
