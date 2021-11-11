63c27a5776e5f37e1056bbd5c5982ed157fa5fa1
Maykon D. Granemann <maykondgranemann@gmail.com>
2021-11-11 14:58:34 -0300

    Ajuste nome arquivos

--- IntroducaoJava/src/modulo1/aula33/view/View.java
+++ IntroducaoJava/src/modulo1/aula33/view/View.java
@@ -2,33 +2,27 @@ package view;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
-public class View {
+public class View{
     public static void main(String[] args) {
         try {
-            Connection conn = DriverManager.getConnection("jdbc:postgresql://192.168.0.18:5432/postgres", "postgres", "123456");
-            
-            // Statement - query SQL
-            Statement statement = conn.createStatement();
-            statement.execute("SELECT * FROM pessoa");
-            // Result = retorno da consulta
-            ResultSet result = statement.getResultSet();
-            
-            // Imprimir resultado
-            while(result.next()){
-                int id = result.getInt("id");
-                String nome = result.getString("nome");
-                String sobrenome = result.getString("sobrenome");
-                System.out.printf("%d - %s %s\n", id, nome, sobrenome);
-            }
+            String driverType = "jdbc";
+            String driverName = "postgresql";
+            String host = "192.168.0.18";
+            int port = 5432;
+            String database = "postgres";
+            String user = "postgres";
+            String pwd = "123456";
+
+            String connectionString = String.format("%s:%s://%s:%d/%s", driverType, driverName, host, port, database);
+
+            Connection conn =  DriverManager.getConnection(connectionString, user, pwd);
 
             conn.close();
+
         } catch (SQLException e) {
-            //System.out.println("Nao foi possivel conectar");
-            e.printStackTrace();
+            System.out.println("Nao foi possivel conectar");
         }
     }
 }
--- IntroducaoJava/src/modulo1/aula33/view/View2.java
+++ IntroducaoJava/src/modulo1/aula33/view/View2.java
@@ -2,27 +2,33 @@ package view;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
-public class View2{
+public class View2 {
     public static void main(String[] args) {
         try {
-            String driverType = "jdbc";
-            String driverName = "postgresql";
-            String host = "192.168.0.18";
-            int port = 5432;
-            String database = "postgres";
-            String user = "postgres";
-            String pwd = "123456";
-
-            String connectionString = String.format("%s:%s://%s:%d/%s", driverType, driverName, host, port, database);
-
-            Connection conn =  DriverManager.getConnection(connectionString, user, pwd);
+            Connection conn = DriverManager.getConnection("jdbc:postgresql://192.168.0.18:5432/postgres", "postgres", "123456");
+            
+            // Statement - query SQL
+            Statement statement = conn.createStatement();
+            statement.execute("SELECT * FROM pessoa");
+            // Result = retorno da consulta
+            ResultSet result = statement.getResultSet();
+            
+            // Imprimir resultado
+            while(result.next()){
+                int id = result.getInt("id");
+                String nome = result.getString("nome");
+                String sobrenome = result.getString("sobrenome");
+                System.out.printf("%d - %s %s\n", id, nome, sobrenome);
+            }
 
             conn.close();
-
         } catch (SQLException e) {
-            System.out.println("Nao foi possivel conectar");
+            //System.out.println("Nao foi possivel conectar");
+            e.printStackTrace();
         }
     }
 }
