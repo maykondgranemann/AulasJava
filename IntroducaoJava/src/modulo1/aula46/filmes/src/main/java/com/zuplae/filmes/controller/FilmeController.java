14065757cd9ca496f384d78e4b1d9dcf9fc01f4e
Maykon  Granemann <maykondgranemann@gmail.com>
2021-11-29 10:29:03 -0300

    Aula 46 - Controller - JPA - Listagem

--- IntroducaoJava/src/modulo1/aula46/filmes/src/main/java/com/zuplae/filmes/controller/FilmeController.java
+++ IntroducaoJava/src/modulo1/aula46/filmes/src/main/java/com/zuplae/filmes/controller/FilmeController.java
@@ -1,16 +1,28 @@
 package com.zuplae.filmes.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
+
+import com.zuplae.filmes.model.Filme;
+import com.zuplae.filmes.repository.FilmeRepository;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FilmeController {
+    private FilmeRepository repository;
+
+    public FilmeController(FilmeRepository repository) {
+        this.repository = repository;
+    }
 
     @GetMapping("/filme")
     public String filmes(HttpServletRequest req){
-        req.setAttribute("nome", "Esqueceram de mim");
+        List<Filme> lista = (List<Filme>)repository.findAll();
+        req.setAttribute("filmes", lista);
         return "filmes";
+        
     }
 }
