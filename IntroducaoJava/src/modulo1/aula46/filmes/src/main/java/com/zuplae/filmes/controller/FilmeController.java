ede94457a5d3e3641183e25cdffb4e8eeec5be00
Maykon  Granemann <maykondgranemann@gmail.com>
2021-11-29 11:15:28 -0300

    Aula 46 - Delete - Update - UrlParameters databind

--- IntroducaoJava/src/modulo1/aula46/filmes/src/main/java/com/zuplae/filmes/controller/FilmeController.java
+++ IntroducaoJava/src/modulo1/aula46/filmes/src/main/java/com/zuplae/filmes/controller/FilmeController.java
@@ -2,13 +2,13 @@ package com.zuplae.filmes.controller;
 
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-
 import com.zuplae.filmes.model.Filme;
 import com.zuplae.filmes.repository.FilmeRepository;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -20,15 +20,17 @@ public class FilmeController {
     }
 
     @GetMapping("/filme")
-    public String filmes(HttpServletRequest req){
+    public String filmes(Model req){
         List<Filme> lista = (List<Filme>)repository.findAll();
-        req.setAttribute("filmes", lista);
+        req.addAttribute("filmes", lista);
         return "filmes";
         
     }
 
     @GetMapping("/filme/form")
-    public String formulario(){
+    public String formulario(Model req){
+        Filme model = new Filme();
+        req.addAttribute("filme", model);
         return "filmes-form";
     }
 
@@ -36,5 +38,18 @@ public class FilmeController {
     public String salvar(Filme model){
         repository.save(model);
         return "redirect:/filme";
+    }
+
+    @GetMapping("/filme/deletar/{id}")
+    public String deletar(@PathVariable int id){
+        repository.deleteById(id);
+        return "redirect:/filme";
+    }
+
+    @GetMapping("/filme/editar/{id}")
+    public String editar(@PathVariable int id, Model req){
+        Filme model = repository.findById(id).get();
+        req.addAttribute("filme", model);
+        return "filmes-form";
     }
 }
